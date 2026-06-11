359e50069a5facb9ba34608bb98436c4211de9b6
rahishree79 <rahishreeshah497@gmail.com>
2026-06-11 14:12:35 +0530

    fix CORS for production

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const orderRoutes = require('./routes/orderRoutes');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: '*', credentials: false }));
 app.use(express.json());
 app.use(morgan('dev'));
 
